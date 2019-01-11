c248ed2275e28f1fc1a22138f7e079e156631b44
LinJK <linjk@zhenglaikeji.com>
2019-01-11 13:43:10 +0800

    使用模板创建原生菜单

--- index.js
+++ index.js
@@ -1,9 +1,67 @@
-const {app, BrowserWindow} = require('electron');
+// 打包工具: npm install electron-packager -g
+// 发布(electron -v): electron-packager . TestElectron --electron-version=4.0.1
+
+const {app, BrowserWindow, Menu} = require('electron');
+
+const template = [
+	{
+		label: '文件',
+		submenu: [
+			{
+				label: '关于',
+				role: 'about',
+				click: () => {
+					var aboutWin = new BrowserWindow({width:300, height: 200, parent: win, modal: true});
+					aboutWin.loadFile('https://www.baidu.com');
+				}
+			},
+			{
+				type: 'separator'
+			},
+			{
+				label: '关闭',
+				accelerator: 'Command+Q',
+				click: () => {
+					win.close();
+				}
+			}
+		]
+	},
+	{
+		label: '编辑',
+		submenu: [
+			{
+				label: '复制',
+				click: () => {win.webContents.insertText('复制')}
+			},
+			{
+				label: '剪切',
+				click: () => {win.webContents.insertText('剪切')}
+			},
+			{
+				type: 'separator'
+			},
+			{
+				label: '查找',
+				accelerator: 'Command+F',
+				click: () => {win.webContents.insertText('查找')}
+			},
+			{
+				label: '替换',
+				accelerator: 'Command+R',
+				click: () => {win.webContents.insertText('替换')}
+			}
+		]
+	}
+];
 
 function createWindow() {
 	win = new BrowserWindow({width: 800, height: 600});
 	win.webContents.openDevTools();                        // 打开调试窗口
 	win.loadFile('index.html')
+	// 创建菜单对象
+	const menu = Menu.buildFromTemplate(template);
+	Menu.setApplicationMenu(menu);
 	win.on('closed', () => {
 		console.log('closed');
 		win = null;
